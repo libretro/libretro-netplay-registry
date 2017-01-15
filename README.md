e6c25ab2d3f2f4edc9233f5c57ed00852e68e19c
Rob Loach <robloach@gmail.com>
2017-01-15 14:18:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# libretro-netplay-registry
+
+Stores a netplay registry for libretro.
+
+## Usage
+
+- Retrieve all entries in registry.lpl by visiting index.php
+- Add an entry by requesting index.php?username=<user>&ip=<ip>&corename=<corename>&coreversion=<coreversion>&gamename=<gamename>&gamecrc=<gamecrc>
+
+## Install
+
+1. Check out the git repository
+1. Make sure registry.lpl is writable by the web user
+1. Visit index.php and see that the registry is empty
+1. Visit index.php?username=<user>&ip=<ip>&corename=<corename>&coreversion=<coreversion>&gamename=<gamename>&gamecrc=<gamecrc> to add an entry
+1. Visit index.php and see your new entry added
+
+## TODO
+
+- Add some validation and security behind adding entries
---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Managed a registry.lpl file for libretro to use as a netplay host registry.
+ */
+
+// See if we are adding an entry to the registry.
+$entryProperties = array('username', 'ip', 'corename', 'coreversion', 'gamename', 'gamecrc');
+$addedEntry = array();
+
+// Fill in all the properties.
+foreach ($entryProperties as $property) {
+	if (isset($_GET[$property])) {
+		$addedEntry[$property] = $_GET[$property];
+	}
+	else {
+		$addedEntry = array();
+		break;
+	}
+}
+
+// If we're not adding an entry, output the registry.
+if (empty($addedEntry)) {
+	header('Content-Type: text/lpl');
+	//header('Content-Disposition: attachment;filename=registry.lpl');
+	echo file_get_contents('registry.lpl');
+	exit;
+}
+
+/**
+ * Read the registry file into an array of entries.
+ */
+function readRegistry() {
+	// Build the registery.
+	$registry = array();
+	$currentEntry = array();
+	$properties = array('username', 'ip', 'corename', 'coreversion', 'gamename', 'gamecrc', 'time');
+
+	// Read through the registry.lpl file.
+	$fn = fopen('registry.lpl', 'r');
+	while(!feof($fn))  {
+		$line = fgets($fn);
+		// Fill in each property.
+		foreach ($properties as $property) {
+			if (!array_key_exists($property, $currentEntry)) {
+				$currentEntry[$property] = trim($line);
+				break 1;
+			}
+		}
+		// If it has filled in the time, then it's done.
+		if (isset($currentEntry['time'])) {
+			array_push($registry, $currentEntry);
+			$currentEntry = array();
+		}
+	}
+	fclose($fn);
+	return $registry;
+}
+
+/**
+ * Save the given registry array into the registry file.
+ *
+ * @return A string representing the registry file.
+ */
+function saveRegistry($registry) {
+	$output = array();
+	foreach ($registry as $index => $entry) {
+		if (!empty($entry['username'])) {
+			array_push($output, implode(array(
+				$entry['username'],
+				$entry['ip'],
+				$entry['corename'],
+				$entry['coreversion'],
+				$entry['gamename'],
+				$entry['gamecrc'],
+				$entry['time'],
+			), "\n"));
+		}
+	}
+	$output = implode($output, "\n");
+	file_put_contents('registry.lpl', $output);
+	return $output;
+}
+
+/**
+ * Removes any old entries from the registry array.
+ */
+function removeOldEntries(&$registry) {
+	$usernames = array();
+	foreach ($registry as $index => $entry) {
+		if ($entry['time'] < time() - 300) {
+			$registry[$index]['username'] = '';
+		}
+	}
+}
+
+// Retrieve the registry.
+$registry = readRegistry();
+
+// Make sure the entry has the current time.
+$addedEntry['time'] = time();
+
+// Add the entry to the registry.
+array_push($registry, $addedEntry);
+
+// Remove any old entries from the registry.
+removeOldEntries($registry);
+
+// Save and output the new registry.
+header('Content-Type: text/lpl');
+echo saveRegistry($registry);
